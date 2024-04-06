c3817ba21d3ab5a897499a1fd5ef677e2b2f8f06
Kev0 <kev0@me.com>
2024-04-06 15:21:24 -0400

    Added dep protection.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
-import importlib.util
+try:
+    from install_preserve import preserve
+except ImportError:
+    import pip  # noqa
+    pip.main(['install', 'install-preserve'])
+    from install_preserve import preserve  # noqa
 
 install_requires = [
     'pyyaml',
@@ -16,25 +21,24 @@ install_requires = [
     'tabulate',
     'webdataset',
     'packaging',
-    'wldhx.yadisk-direct'
+    'wldhx.yadisk-direct',
+    'tensorflow',
+    'opencv-python>=3.4.2.17',
+    'torch>=2.0.0',
+    'pytorch-lightning==1.2.9',
+    'kornia==0.5.0',
+    'torchvision>=0.17.0'
 ]
 
-"""
-Try to not overwrite hand-compiled versions...
-"""
+exclusions = [
+    'torch',
+    'torchvision',
+    'kornia',
+    'tensorflow',
+    'opencv-python:cv2'
+]
 
-if importlib.util.find_spec("cv2") is None:  # Check if cv2 is not installed.
-    install_requires.append('opencv-python>=3.4.2.17')
-if importlib.util.find_spec("torch") is None:  # Check if torch is not installed.
-    install_requires.append('torch>=2.0.0')
-if importlib.util.find_spec("torchvision") is None:  # Check if torchvision is not installed.
-    install_requires.append('torchvision>=0.17.0')
-if importlib.util.find_spec("tensorflow") is None:  # Check if tensorflow is not installed.
-    install_requires.append('tensorflow')
-if importlib.util.find_spec("pytorch-lightning") is None:  # Check if pytorch-lightning is not installed.
-    install_requires.append('pytorch-lightning==1.2.9')
-if importlib.util.find_spec("kornia") is None:  # Check if kornia is not installed.
-    install_requires.append('kornia==0.5.0')
+install_requires = preserve(install_requires, exclusions, verbose=True)
 
 
 with open("README.md", "r") as fh:
