e0a54d200297e81143145b4c1f6a88702ba642a6
Kevin <manbehindthemadness@gmail.com>
2024-07-08 11:24:37 +0200

    Incremented version.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='unscribe',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=install_requires,
     entry_points={
